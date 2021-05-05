42562fee968d22b979eeabb6d29111a5021cdaa1
Wendel Alves <wendel.alves@academico.ifpb.edu.br>
2021-05-05 14:50:03 -0300

    Adicionando os dois primeiros testes unitários

--- post_app/tests.py
+++ post_app/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Noticia
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class NewsTestCase(TestCase): #Teste Unitário Cadastrar Noticia
+
+    def setUp(self):
+
+        usuario = User.objects.create(
+            username='Teste',
+            password='123456'
+        )
+
+        Noticia.objects.create(
+            codigo=1,
+            titulo="Titulo da Noticia",
+            header_image="",
+            assunto="Assunto da noticia",
+            texto="Texto da Noticia",
+            timestamp="",
+            author=usuario,
+        )
+
+    def test_cadastrar_noticia(self):
+        noticia = Noticia.objects.get(codigo=1)
+        self.assertEquals(noticia.titulo, 'Titulo da Noticia')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class UnitTestsUser(TestCase):
+
+    def setUp(self):
+
+        User.objects.create(
+            username='Teste',
+            password='123456'
+        )
+
+    def test_cadastro_user(self):
+        user = User.objects.get(username='Teste')
+        self.assertEquals(user.username, 'Teste')
+
+
+
